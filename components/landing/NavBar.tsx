96b83d751578bc1aaeff87fd588c428d4cb06f05
Melzira Ebo <mebo@c3r3s3.42luanda.com>
2026-05-11 18:04:33 +0100

    first coloring and componenting

--- components/landing/NavBar.tsx
+++ components/landing/NavBar.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function NavBar()
 {
     return (
-        <nav className="fixed rounded-full p-6 flex gap-10 bg-navbar text-white">
-            <div className="">
+        <nav className="fixed top-4 shadow-card left-1/2 -translate-x-1/2 rounded-full p-5 flex items-center gap-10 bg-navbar text-white">
+            <div className="px-8">
                 <h1>Vectoria logo</h1>
             </div>
             <div className="flex gap-4">
@@ -15,7 +15,7 @@ export default function NavBar()
                 <a href="#contribute">Contribua</a>
             </div>
             <div className="flex">
-                <Link href="/app" className="bg-button shadow-btn hover:shadow-btn-active text-white px-4 py-2 rounded-lg">
+                <Link href="/app" className="bg-button shadow-btn hover:shadow-btn-active text-white px-4 py-1 text-center rounded-lg">
                     Acessar
                 </Link>
             </div>
